a0adfc4a881774da4fe83d569acc762c3082509e
HuuHieu2005 <hieub2306615@student.ctu.edu.vn>
2026-07-06 19:40:05 +0700

    cai dat express chay thong bao xin chao

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+app.use(cors());
+app.use(express.json());
+app.get ('/', (req, res) => {
+    res.json({ message: 'Welcome to the ContactBook application.' });
+});
+module.exports = app;
