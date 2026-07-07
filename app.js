65f320150bd464b84f0f00209255c6728e43b17d
HuuHieu2005 <hieub2306615@student.ctu.edu.vn>
2026-07-07 21:39:31 +0700

    Dinh nghia cac route cho tai nguyen Contact

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const contactRoutes = require('./app/routes/contact.route');
 app.use(cors());
 app.use(express.json());
 app.get ('/', (req, res) => {
     res.json({ message: 'Welcome to the ContactBook application.' });
 });
+app.use('/api/contacts', contactRoutes);
 module.exports = app;
