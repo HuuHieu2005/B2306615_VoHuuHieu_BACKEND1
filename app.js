3a3e61dbb10399ae348f40eac4c40a5be888c24e
HuuHieu2005 <hieub2306615@student.ctu.edu.vn>
2026-07-10 16:41:14 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,25 +1,26 @@
-const express = require('express');
-const ApiError = require('./app/api-error');
+const express = require("express");
+const cors = require("cors");
+const ApiError = require("./app/api-error");
+const contactRoutes = require("./app/routes/contact.route");
+
 const app = express();
-const cors = require('cors');
-const contactRoutes = require('./app/routes/contact.route');
-//handle 404 error
-app.use((req, res, next) => {
-    // code o day se chay khi khong co route duoc dinh nghia nao
-    // khop voi yeu cau goi next() de chuyen sang middleware xu ly loi
-    return next(new ApiError(404, 'Resource not found'));
-});
-// define error-handling middleware last, after other app.use() and routes calls
-app.use((err, req, res, next) => {
-    //middleware xu ly loi tap trung
-    return res.status(err.statusCode || 500).json({
-        message: err.message || 'Internal Server Error',
-    });
-});
 app.use(cors());
 app.use(express.json());
-app.get ('/', (req, res) => {
-    res.json({ message: 'Welcome to the ContactBook application.' });
+
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to the ContactBook application." });
 });
-app.use('/api/contacts', contactRoutes);
+
+app.use("/api/contacts", contactRoutes);
+
+app.use((req, res, next) => {
+  return next(new ApiError(404, "Resource not found"));
+});
+
+app.use((err, req, res, next) => {
+  return res.status(err.statusCode || 500).json({
+    message: err.message || "Internal Server Error",
+  });
+});
+
 module.exports = app;
